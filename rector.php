e3a7836af07e7788a26927b10a41ca063ac34624
Rémi San <remi.san@gmail.com>
2024-08-04 17:22:22 +0200

    test: fix tests

--- rector.php
+++ rector.php
@@ -2,43 +2,34 @@
 
 declare(strict_types=1);
 
-use Rector\Caching\ValueObject\Storage\FileCacheStorage;
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
 use Rector\DeadCode\Rector\ClassMethod\RemoveEmptyClassMethodRector;
-use Rector\DeadCode\Rector\MethodCall\RemoveEmptyMethodCallRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
+use Rector\ValueObject\PhpVersion;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__.'/src',
         __DIR__.'/tests/unit',
         __DIR__.'/tests/helper',
-    ]);
-
-    $rectorConfig->importNames();
-    $rectorConfig->importShortClasses(false);
-    $rectorConfig->phpVersion(PhpVersion::PHP_82);
-    $rectorConfig->phpstanConfig(__DIR__.'/phpstan.neon');
-
-    $rectorConfig->sets([
+    ])
+    ->withSets([
         SetList::DEAD_CODE,
         SetList::PRIVATIZATION,
         SetList::TYPE_DECLARATION,
         SetList::CODE_QUALITY,
         LevelSetList::UP_TO_PHP_82,
         PHPUnitSetList::PHPUNIT_100,
-    ]);
-
-    $rectorConfig->skip([
+    ])
+    ->withImportNames(importShortClasses: false)
+    ->withPhpVersion(PhpVersion::PHP_82)
+    ->withPHPStanConfigs([__DIR__.'/rector-phpstan.neon'])
+    ->withCache(__DIR__.'/var/tmp/rector')
+    ->withSkip([
         __DIR__.'/tests/unit/Test/Formatter/SimpleTestCaseFormatterTest.php',
         RemoveEmptyClassMethodRector::class => [
             __DIR__.'/tests/unit/Test/SimpleTestSuiteDocumentorTest.php',
         ],
     ]);
-
-    $rectorConfig->cacheClass(FileCacheStorage::class);
-    $rectorConfig->cacheDirectory(__DIR__.'/var/tmp/rector');
-};
